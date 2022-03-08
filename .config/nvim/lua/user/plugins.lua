f0b2921accb3ffc0240c919f6b4db4900a1e3bae
deltakroneker <nikola.milic.ios@gmail.com>
2022-03-08 19:53:34 +0100

    Add nvim LSP

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -50,6 +50,10 @@ return packer.startup(function(use)
     "kyazdani42/nvim-tree.lua",
     requires = { "kyazdani42/nvim-web-devicons", opt = truen }
   }
+
+  use "neovim/nvim-lspconfig"
+  use "williamboman/nvim-lsp-installer"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
